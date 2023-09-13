af4be1c972b2fbf000ff3118ca1a5a8da3af33ce
Daniel Zotti <daniel.zotti87@gmail.com>
2023-09-13 12:11:52 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Push Notifications Local
+
+A simple project for playing with "Push Notification" API (client side only!)
+
+- Install `http-server` package to test it locally: `npm install -g http-server`
+- Run the project: `npm run start` or `http-server` (it will run a server on http://localhost:8080)
---
+++ sw.js
@@ -0,0 +1,47 @@
+console.log('Hello from service worker!')
+
+self.addEventListener('notificationclick', (event) => {
+    console.log(`[ServiceWorker] Notification CLICKED! ${event.notification.title}`);
+    if (event.action) {
+        console.log(`with action: ${event.action}`);
+    }
+    event.notification.close();
+
+    event.waitUntil(
+        clients
+            .matchAll({
+                includeUncontrolled: true, // See: https://stackoverflow.com/questions/35100759/serviceworkers-focus-tab-clients-is-empty-on-notificationclick
+                type: "window",
+            })
+            .then((clientList) => {
+                for (const client of clientList) {
+                    if (client.visibilityState === "visible") {
+                        console.log("[ServiceWorker] webpage already in focus!")
+                        return;
+                    }
+                    if (client.visibilityState === "hidden" && "focus" in client) {
+                        console.log("[ServiceWorker] webpage not in focus, setting focus on it...")
+                        return client.focus();
+                    }
+                }
+                console.log(clientList);
+                debugger;
+                if (clients.openWindow) {
+                    console.log("[ServiceWorker] webpage is close, let's opening it...")
+                    return clients.openWindow("/");
+                }
+            }),
+    );
+});
+
+self.addEventListener('notificationclose', (event) => {
+    console.log(`[ServiceWorker] Notification CLOSED! ${event.notification.title}`)
+});
+
+self.addEventListener('push', function (event) {
+    if (event.data) {
+        self.registration.showNotification("Push Notification", {body: event.data.text()});
+    } else {
+        console.log('[ServiceWorker] Push event but no data')
+    }
+})
